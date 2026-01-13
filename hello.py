1614c0f4982a2ac133d840fc9e0516600fefdfac
Ismael Salazar <90110170+iasalazar1@users.noreply.github.com>
2026-01-13 07:42:41 -0600

    Delete hello.py
    Se me fue "hola mundo"

--- hello.py
+++
@@ -1,8 +0,0 @@
-h = "¡Hola Mundo!"
-print (h)
-print ("La longitud es",len(h))
-print (h.upper())
-print (h.lower())
-print ("El número de l´s es:", h.count("l"))
-print ("La posición del signo !, (contando el 0) es:", h.find("!"))
-print (h.replace("!","?"))
